224a820d0c870dd92dd37504ffa2310a574b1020
‍김용빈 <yongbin97@yonsei.ac.kr>
2024-05-13 21:17:49 +0900

    [FIX]: user id nullable false

--- src/main/java/com/example/springplayground/payments/entity/Payment.java
+++ src/main/java/com/example/springplayground/payments/entity/Payment.java
@@ -20,7 +20,7 @@ public class Payment extends BaseTimeEntity {
     private Long id;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "user_id")
+    @JoinColumn(name = "user_id", nullable = false)
     private User user;
     private String orderId;
     private String orderName;
