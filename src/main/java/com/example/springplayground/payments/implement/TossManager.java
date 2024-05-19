19acb8bd5471aede7de447c59aab6787d5375806
Kim Yongbin <80311590+yongbin97@users.noreply.github.com>
2024-05-19 20:23:16 +0900

    [REFACTOR]: Implement Layer 분리 - TossManager

--- src/main/java/com/example/springplayground/payments/implement/TossManager.java
+++ src/main/java/com/example/springplayground/payments/implement/TossManager.java
@@ -1,11 +1,11 @@
-package com.example.springplayground.payments.service;
+package com.example.springplayground.payments.implement;
 
 import com.example.springplayground.payments.dto.TossConfirmResDto;
 import lombok.extern.slf4j.Slf4j;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Service;
+import org.springframework.stereotype.Component;
 
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -17,18 +17,20 @@ import java.nio.charset.StandardCharsets;
 import java.util.Base64;
 
 @Slf4j
-@Service
-public class TossPaymentsService {
+@Component
+public class TossManager {
     @Value("${payments.toss.secretKey}")
     private String tossSecretKey;
 
-    public TossConfirmResDto requestConfirm(JSONObject paymentJSON) throws Exception {
-        log.info("[INFO] Toss Payments Service - requestConfirm");
+    // Authorizations
+    private String getAuthorizations() {
+        Base64.Encoder encoder = Base64.getEncoder();
+        byte[] encodedBytes = encoder.encode((tossSecretKey + ":").getBytes(StandardCharsets.UTF_8));
+        return "Basic " + new String(encodedBytes);
+    }
 
-        // Authorizations
-        String authorizations = getAuthorizations();
-
-        // 결제 승인 API 호출
+    // 결제 승인 API 호출
+    private HttpURLConnection callConfirmTossAPI(String authorizations, JSONObject paymentJSON) throws Exception {
         URL url = new URL("https://api.tosspayments.com/v1/payments/confirm");
         HttpURLConnection connection = (HttpURLConnection) url.openConnection();
         connection.setRequestProperty("Authorization", authorizations);
@@ -39,9 +41,10 @@ public class TossPaymentsService {
         OutputStream outputStream = connection.getOutputStream();
         outputStream.write(paymentJSON.toString().getBytes(StandardCharsets.UTF_8));
 
-        int responseCode = connection.getResponseCode();
-        boolean isSuccess = responseCode == 200;
+        return connection;
+    }
 
+    private JSONObject getConfirmResult(HttpURLConnection connection, boolean isSuccess) throws Exception {
         InputStream responseStream = isSuccess ? connection.getInputStream() : connection.getErrorStream();
 
         JSONParser parser = new JSONParser();
@@ -49,17 +52,24 @@ public class TossPaymentsService {
         JSONObject jsonObject = (JSONObject) parser.parse(reader);
         responseStream.close();
 
+        return jsonObject;
+    }
+
+    public TossConfirmResDto confirm(JSONObject paymentJSON) throws Exception {
+        log.info("[INFO] Toss Payments Service - confirm");
+
+        String authorizations = getAuthorizations();
+        HttpURLConnection connection = callConfirmTossAPI(authorizations, paymentJSON);
+
+        int responseCode = connection.getResponseCode();
+        boolean isSuccess = responseCode == 200;
+        JSONObject jsonObject = getConfirmResult(connection, isSuccess);
+
         return TossConfirmResDto.builder()
                 .responseCode(responseCode)
                 .obj(jsonObject)
                 .code(isSuccess ? "SUCCESS" : (String) jsonObject.get("code"))
                 .message(isSuccess ? "결제에 성공하였습니다." : (String) jsonObject.get("message"))
                 .build();
-    }
-
-    private String getAuthorizations() {
-        Base64.Encoder encoder = Base64.getEncoder();
-        byte[] encodedBytes = encoder.encode((tossSecretKey + ":").getBytes(StandardCharsets.UTF_8));
-        return "Basic " + new String(encodedBytes);
     }
 }
--- src/main/java/com/example/springplayground/payments/service/PaymentsService.java
+++ src/main/java/com/example/springplayground/payments/service/PaymentsService.java
@@ -3,10 +3,10 @@ package com.example.springplayground.payments.service;
 import com.example.springplayground.payments.dto.PaymentConfirmResDto;
 import com.example.springplayground.payments.dto.TossConfirmResDto;
 import com.example.springplayground.payments.entity.Payment;
+import com.example.springplayground.payments.implement.TossManager;
 import com.example.springplayground.payments.repository.PaymentRepository;
 import com.example.springplayground.user.entity.User;
 import com.example.springplayground.user.implement.UserReader;
-import com.example.springplayground.user.repository.UserRepository;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.json.simple.JSONObject;
@@ -18,15 +18,14 @@ import org.springframework.stereotype.Service;
 @Service
 public class PaymentsService {
     private final UserReader userReader;
-
-    private final TossPaymentsService tossPaymentsService;
+    private final TossManager tossManager;
     private final PaymentRepository paymentRepository;
 
     public PaymentConfirmResDto requestConfirmToTossPayments(Long userId, JSONObject paymentJSON) throws Exception {
         log.info("[INFO] PaymentsService - requestConfirmToTossPayments");
         User user = userReader.read(userId);
 
-        TossConfirmResDto tossConfirmResDto = tossPaymentsService.requestConfirm(paymentJSON);
+        TossConfirmResDto tossConfirmResDto = tossManager.confirm(paymentJSON);
         if (tossConfirmResDto.getResponseCode() != 200) {
             log.error("[ERROR] Toss Payments Service - requestConfirm failed: {}", tossConfirmResDto.getMessage());
             return tossConfirmResDto.toPaymentConfirmResDto();
